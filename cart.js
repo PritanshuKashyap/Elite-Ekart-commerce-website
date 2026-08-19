26ead186d5a8d364c43bbffe0ac772b4dafdbdf5
Pritanshu Kumar <pritanshukumar434@gmail.com>
2026-08-19 14:33:08 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -2,7 +2,7 @@ console.clear();
 
 if(document.cookie.indexOf(',counter=')>=0)
 {
-    let counter = document.cookie.split(',')[1].split('=')[1]
+    let counter = document.cookie.split(',')[1].split('=')[1] 
     document.getElementById("badge").innerHTML = counter 
 }
 
