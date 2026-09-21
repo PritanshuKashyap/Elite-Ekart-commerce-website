4c0750bc9c127b7fa7986bf95851eb3f214133a6
Pritanshu Kumar <pritanshukumar434@gmail.com>
2026-09-21 22:16:04 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,6 +1,6 @@
 console.clear();
 
-if(document.cookie.indexOf(',counter=')>=0)
+if(document.cookie.indexOf(',counter=')>=0) 
 {  
     let counter = document.cookie.split(',')[1].split('=')[1] 
     document.getElementById("badge").innerHTML = counter 
